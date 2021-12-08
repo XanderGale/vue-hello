a28350c335b2d001a546c5a629c7f3685db7f64c
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-08 19:41:54 +0100

    1st step

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+const newapp = new Vue(
+    {
+        el: '#container',
+        data: {
+            textTitle: 'Il tuo messaggio qui'
+        }
+    }
+);
